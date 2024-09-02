dd79d82774302f280298161b7a402adb6a51c0bf
Lucas Araujo <lucas araujo@spcombustiveis.local>
2024-09-02 08:48:34 -0300

    02/09/2024 - Arquivos do curso JS e TS

---
+++ js/aula 10 - let vs var - primeira diferença/src/js/letVSvar.js
@@ -0,0 +1,5 @@
+var nome = "luiz"; //uso antiquado
+let nome = "sergipe"; //uso atual
+nome = "johny"; // uso incorreto, sucetivel a bugar as variaveis pois a variavel nome fica global atrapalhando assim o sistema
+
+console.log(nome);

78084b1081b3ac9e32881e5d4156d07e6d371bbb
Lucas Muniz <114953204+lucaramuniz@users.noreply.github.com>
2024-10-28 17:10:04 -0300

    28/10 - Update de novas aulas

---
+++ modulo 1 - Javascript Básico - Para Iniciantes/aula 013 - Alert, confirm e prompt (Navegador)/src/js/navegador.js
@@ -0,0 +1,17 @@
+window.alert("Mensagem");
+window.confirm("Deseja realmente apagar?");
+confirm("Deseja realmente apagar?");
+window.prompt("Digite seu nome");
+console.log("olá mundo");
+
+const confirm = window.confirm("Deseja realmente apagar?");
+let confirmar = window.confirm("Qualquer Mensagem");
+let digiteNumero_1 = prompt("Digite um número");
+let digiteNumero_2 = prompt("Digite outro número");
+
+
+typeof digiteNumero_1
+digiteNumero_1 = parseFloat(digiteNumero_1);
+
+const resultado = digiteOperador_1 + digiteOperador_2;
+window.alert(`O digite o número ${resultado}`);
---
+++ modulo 1 - Javascript Básico - Para Iniciantes/aula 014  - Arrays (Básico)/arrays.js
@@ -0,0 +1,11 @@
+const alunos = ["Endrinho", "Luquinha", "Wellitu","Viniciuuss"];
+console.log(alunos);
+console.log(alunos[0]);
+console.log(alunos[1]);
+
+const motorista = [ ];
+motorista[0] = "Rennan de Almeida";
+motorista[1] = "Maurilio dos Anjos";
+motorista[2] = "Rogerinho do Ingá";
+motorista[3] = "Julinho da Van";
+motorista.length("Rennanzinho");

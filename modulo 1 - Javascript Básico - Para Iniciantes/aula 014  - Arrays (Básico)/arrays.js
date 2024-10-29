8da5c140134f51c8cfa2523c9a4614898a01f95b
Lucas Muniz <114953204+lucaramuniz@users.noreply.github.com>
2024-10-29 15:58:31 -0300

    29/10/24 - Update das aulas

--- modulo 1 - Javascript Básico - Para Iniciantes/aula 014  - Arrays (Básico)/arrays.js
+++ modulo 1 - Javascript Básico - Para Iniciantes/aula 014  - Arrays (Básico)/arrays.js
@@ -3,9 +3,14 @@ console.log(alunos);
 console.log(alunos[0]);
 console.log(alunos[1]);
 
-const motorista = [ ];
-motorista[0] = "Rennan de Almeida";
-motorista[1] = "Maurilio dos Anjos";
-motorista[2] = "Rogerinho do Ingá";
-motorista[3] = "Julinho da Van";
-motorista.length("Rennanzinho");
+alunos[0] = "Rennan de Almeida";
+alunos[1] = "Maurilio dos Anjos";
+alunos[2] = "Rogerinho do Ingá";
+alunos[3] = "Julinho da Van";
+alunos.length("Rennanzinho");
+
+alunos.push("luiza");
+alunos.unshift("endrinho");
+alunos.pop();
+alunos.shift();
+

e05850b108106c89322213d3191b5f3dad54a513
Lucas Muniz <114953204+lucaramuniz@users.noreply.github.com>
2024-11-01 16:35:13 -0300

    01/11/24 - Update de aulas

--- modulo 1 - Javascript Básico - Para Iniciantes/aula 017 - exercicio 3/src/forms.js
+++ modulo 1 - Javascript Básico - Para Iniciantes/aula 017 - exercicio 3/src/forms.js
@@ -1,32 +1,42 @@
-function identidade(){
-    const form = window.document.querySelector('.form');
-    const resultado = window.document.querySelector('.resultado');
+function identidade() {
+    const form = window.document.querySelector(".form");
+    const resultado = window.document.querySelector(".resultado");
     const pessoas = [];
-
+  
     //antiga forma
-/*    form.onsubmit = function(event){
-        event.preventDefault();
-        window.alert(1);
-        console.log("Foi enviado!");
-    };
-*/
+    /*    form.onsubmit = function(event){
+          event.preventDefault();
+          window.alert(1);
+          console.log("Foi enviado!");
+      };
+  */
     //forma atual
-//    let cont = 1;
-    function receptEventForm(event){
-        event.preventDefault();
-        const nome = form.querySelector('.nome');
-        const sobrenome = form.querySelector('.sobrenome');
-        const peso = form.querySelector('.peso');
-        const altura = form.querySelector('.altura');
-
-/*
-    console.log(nome.value, sobrenome.value, peso.value, altura.value);
-    event.preventDefault();
-        console.log(`${cont} Form não foi enviado!`);
-        cont++;
-*/    
-}
+    //    let cont = 1;
+    function receptEventForm(event) {
+      event.preventDefault();
+      const nome = form.querySelector(".nome");
+      const sobrenome = form.querySelector(".sobrenome");
+      const peso = form.querySelector(".peso");
+      const altura = form.querySelector(".altura");
+  
+      pessoas.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value,
+      });
+      console.log(pessoas);
+      resultado.innerHTML +=
+        `<p>${nome.value} ${sobrenome.value} ` +
+        `${peso.value}kg ${altura.value}m <p>`;
+      /*
+      console.log(nome.value, sobrenome.value, peso.value, altura.value);
+      event.preventDefault();
+          console.log(`${cont} Form não foi enviado!`);
+          cont++;
+  */
+    }
     form.addEventListener("submit", receptEventForm);
-
-}
-identidade();
+  }
+  identidade();
+  
---
+++ modulo 2 - Javascript - Lógica de Programação/aula 001 - Extensões que se utiliza no VSCode/extensoes.md
@@ -0,0 +1,17 @@
+##Extensões que são úteis
+- Code Runner:
+    Extensão para executar ferramentas e scripts das linguagens de programação
+
+- Code Time:
+    Extensão para definição de métrica de tempo para codificar
+
+- EJS Language Support:
+    Extensão para renderização do HTML
+    Séra utilizado nas próximas
+- Git Graph
+    Extensão para visualizar melhor seu versionamentos de códigos no Git
+
+- StandardJS -  Javascript Standard Style:
+    Extensão de padronização de códigos javascript
+    Não recomendável para quem está iniciado
+

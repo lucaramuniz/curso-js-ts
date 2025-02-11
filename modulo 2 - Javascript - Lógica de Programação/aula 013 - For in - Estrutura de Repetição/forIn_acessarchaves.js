e4d5fd01cc608c67f612583fc92fa4b10020c1cc
Lucas Muniz <114953204+lucaramuniz@users.noreply.github.com>
2025-02-11 15:29:36 -0300

    11/02/25 - Update de aulas

---
+++ modulo 2 - Javascript - Lógica de Programação/aula 013 - For in - Estrutura de Repetição/forIn_acessarchaves.js
@@ -0,0 +1,15 @@
+const pessoa = {
+    nome: "Lucas",
+    sobrenome: "Santos",
+    idade: 30,
+  };
+  console.log(pessoa.nome);
+  console.log(pessoa["sobrenome"]);
+  
+  const chaves = "nome";
+  console.log(pessoa[chaves]);
+  
+  for (let chave in pessoa) {
+    console.log(`${chave}: ${pessoa[chave]}`);
+  }
+  

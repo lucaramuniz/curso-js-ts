3acd3a55c27a3d60c91be656af60e4bfcf8a92ff
Lucas Muniz <114953204+lucaramuniz@users.noreply.github.com>
2025-03-10 17:15:37 -0300

    10/03/25 - Update de aulas V2

--- modulo 2 - Javascript - Lógica de Programação/aula 024 - Exercicio - Criando uma lista de tarefas/index.js
+++ modulo 2 - Javascript - Lógica de Programação/aula 024 - Exercicio - Criando uma lista de tarefas/index.js
@@ -1,10 +1,10 @@
 const elements = [
-    {tag : "h1", text: "Lista de Tarefas"},
-    {tag : "p", text: "", type:'text'},
-    {tag : "input",text: "", class: "input-tarefa"},
-    {tag : "button",text: "Adicionar uma tarefa", class:"btn-tarefa"},
-    {tag : "ul", text:"", class:"tarefas"},
-]
+  { tag: "h1", text: "Lista de Tarefas" },
+  { tag: "p", text: "", type: "text" },
+  { tag: "input", text: "", class: "input-tarefa" },
+  { tag: "button", text: "Adicionar uma tarefa", class: "btn-tarefa" },
+  { tag: "ul", text: "", class: "tarefas" },
+];
 
 const container = document.querySelector(".container");
 
@@ -12,26 +12,26 @@ const div = document.createElement("div");
 let pElement; // Variável para armazenar o elemento <p>
 
 for (let i = 0; i < elements.length; i++) {
-    let { tag, text, class: className, type } = elements[i];
-    let createTag = document.createElement(tag);
-    if (text) {
-        createTag.textContent = text;
-    }
-    if (className) {
-        createTag.className = className;
-    }
-    if (type) {
-        createTag.type = type;
-    }
+  let { tag, text, class: className, type } = elements[i];
+  let createTag = document.createElement(tag);
+  if (text) {
+    createTag.textContent = text;
+  }
+  if (className) {
+    createTag.className = className;
+  }
+  if (type) {
+    createTag.type = type;
+  }
 
-    if (tag === 'p') { 
-        pElement = createTag; // Guarda o elemento <p> na variável
-        div.appendChild(pElement); // Adiciona o <p> ao div
-    } else if (pElement) {
-        pElement.appendChild(createTag); // Adiciona os elementos seguintes ao <p>
-    } else {
-        div.appendChild(createTag); // Adiciona o <h1> ao div
-    }
+  if (tag === "p") {
+    pElement = createTag; // Guarda o elemento <p> na variável
+    div.appendChild(pElement); // Adiciona o <p> ao div
+  } else if (pElement) {
+    pElement.appendChild(createTag); // Adiciona os elementos seguintes ao <p>
+  } else {
+    div.appendChild(createTag); // Adiciona o <h1> ao div
+  }
 }
 
 container.appendChild(div);
@@ -40,46 +40,45 @@ const tarefaInput = document.querySelector(".input-tarefa");
 const tarefaBtn = document.querySelector(".btn-tarefa");
 const tarefas = document.querySelector(".tarefas");
 
-
 function liCria() {
-    const li = document.createElement('li');
-    return li;
-    
+  const li = document.createElement("li");
+  return li;
 }
-tarefaInput.addEventListener('keypress', function(e){
-    if(e.keyCode === 13){
-        if(!tarefaInput.value) return;
-        tarefaCria(tarefaInput.value);
-        tarefaInput.value = '';
-    }
-})
+tarefaInput.addEventListener("keypress", function (e) {
+  if (e.keyCode === 13) {
+    if (!tarefaInput.value) return;
+    tarefaCria(tarefaInput.value);
+    tarefaInput.value = "";
+  }
+});
 function botaoCriaApagar(li) {
-    li.innertext += ' ';
-    const botaoApagar = document.createElement('button');
-    botaoApagar.innerText = 'Apagar';
-    botaoApagar.classList.add('Apagar');
-    botaoApagar.setAttribute('class','apagar');
-    botaoApagar.setAttribute('title','Apagar esse topico')
-    li.appendChild(botaoApagar);
+  li.innertext += " ";
+  const botaoApagar = document.createElement("button");
+  botaoApagar.innerText = "Apagar";
+  botaoApagar.classList.add("Apagar");
+  botaoApagar.setAttribute("class", "apagar");
+  botaoApagar.setAttribute("title", "Apagar esse topico");
+  li.appendChild(botaoApagar);
 }
 
 function tarefaLimpa(textoInput) {
-    tarefaInput.value = '';
-    tarefaInput.focus();
-    
+  tarefaInput.value = "";
+  tarefaInput.focus();
 }
 
 function tarefaCria(textoInput) {
-    const li = liCria();
-    li.innerText = textoInput;
-    tarefas.appendChild(li);
-    tarefaLimpa();
-    botaoCriaApagar(li);
+  const li = liCria();
+  li.innerText = textoInput;
+  tarefas.appendChild(li);
+  tarefaLimpa();
+  botaoCriaApagar(li);
 }
-tarefaBtn.addEventListener('click',()=>{if(!tarefaInput.value) return;tarefaCria(tarefaInput.value);
-    tarefaInput.value = '';});
+tarefaBtn.addEventListener("click", () => {
+  if (!tarefaInput.value) return;
+  tarefaCria(tarefaInput.value);
+  tarefaInput.value = "";
+});
 
-document.addEventListener('click',(e)=>{
-    const el = e.target;
-
+document.addEventListener("click", (e) => {
+  const el = e.target;
 });
